6e6a79a6b3817e7b1d65280836d347f0aa917932
abhidy <abhidy@yahoo.co.in>
2018-02-09 10:27:30 +0530

    Error Rectified
    Error Rectified

--- PrimeUsingIterations.py
+++ PrimeUsingIterations.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
- for OuterForLoopCounter in range(1,varForLoop):
-	 PrimeNumberFlag = 0
-	 for InnerForLoopCounter in range(2,OuterForLoopCounter):
-		 if(OuterForLoopCounter%InnerForLoopCounter==0):
-			 PrimeNumberFlag = PrimeNumberFlag + 1
-	
-	 if(PrimeNumberFlag > 0):
-		 print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tNotPrime")
-	 else:
-		 print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tPrime")
+varForLoop = 100
+
+for OuterForLoopCounter in range(1,varForLoop):
+	PrimeNumberFlag = 0
+	for InnerForLoopCounter in range(2,OuterForLoopCounter):
+		if(OuterForLoopCounter%InnerForLoopCounter==0):
+			PrimeNumberFlag = PrimeNumberFlag + 1
+
+	if(PrimeNumberFlag > 0):
+		print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tNotPrime")
+	else:
+		print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tPrime")
