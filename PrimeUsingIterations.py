24388f779a87860793bbb256b88199c7b616a9fd
abhidy <abhidy@yahoo.co.in>
2018-02-09 11:26:41 +0530

    Adding date time
    Adding date time

--- PrimeUsingIterations.py
+++ PrimeUsingIterations.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 
 import sys
+import time;
+
+
 
 arguments = sys.argv[1:]
 count = len(arguments)
 
 varForLoop = int(arguments[0])
 
+localtime1 = time.asctime( time.localtime(time.time()) )
 for OuterForLoopCounter in range(1,varForLoop):
 	PrimeNumberFlag = 0
 	for InnerForLoopCounter in range(2,OuterForLoopCounter):
 		if(OuterForLoopCounter%InnerForLoopCounter==0):
 			PrimeNumberFlag = PrimeNumberFlag + 1
+			break
 
-	if(PrimeNumberFlag > 0):
-		print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tNotPrime")
-	else:
+	#if(PrimeNumberFlag > 0):
+		#print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tNotPrime")
+	if(PrimeNumberFlag == 0):
 		print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tPrime")
+		
+localtime = time.asctime( time.localtime(time.time()) )
+print ("Local current time Start:", localtime1)
+print ("Local current time End:", localtime)	
