50d4aaaf5f0afd2fa4620e4fd6f49fe11ac0f082
abhidy <abhidy@yahoo.co.in>
2018-02-09 10:04:16 +0530

    Create ListExample.py

---
+++ ListExample.py
@@ -0,0 +1,8 @@
+# List Tutorial
+
+list = ['physics', 'chemistry', 1997, 2000];
+print ("Value available at index 2 : ")
+print list[2]
+list[2] = 2001;
+print ("New value available at index 2 : ")
+print list[2]
