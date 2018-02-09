bddb072199595701ffe6118426346fd188221b69
abhidy <abhidy@yahoo.co.in>
2018-02-09 10:21:16 +0530

    Prime Number added
    Prime Number added

---
+++ PrimeUsingIterations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+ for OuterForLoopCounter in range(1,varForLoop):
+	 PrimeNumberFlag = 0
+	 for InnerForLoopCounter in range(2,OuterForLoopCounter):
+		 if(OuterForLoopCounter%InnerForLoopCounter==0):
+			 PrimeNumberFlag = PrimeNumberFlag + 1
+	
+	 if(PrimeNumberFlag > 0):
+		 print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tNotPrime")
+	 else:
+		 print ("\tCurrent Value of For Counter\t",OuterForLoopCounter, "\tPrime Status\tPrime")
